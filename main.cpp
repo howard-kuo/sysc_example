ca08a8435556abb06561fa11cbdf0e0de48a9135
howard-kuo <tzxkuo@gmail.com>
2025-01-27 01:02:00 +0800

    first commit for build

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+//main.cpp
+#include "systemc.h"
 
+class HELLO : public sc_module{ 
+public: 
+        HELLO(sc_module_name name) : sc_module(name){ 
+                cout<<"Hello, SystemC!"<<endl; 
+        }
+};
+
+int sc_main(int argc,char** argv){
+        HELLO hello("hello");
+        return 0;
+}
---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+g++ -o systemc_example main.cpp -I/home/howardkuo/git/systemc/install/include -L/home/howardkuo/git/systemc/install/lib -lsystemc -lm
